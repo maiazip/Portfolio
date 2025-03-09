acb027f99da047b383e524a9b5a142fe0e1228dc
Matheus Maia Bastos <maia.zip@hotmail.com>
2025-03-09 20:49:17 -0300

    small fix

--- web/src/app/intro/carousel.tsx
+++ web/src/app/intro/carousel.tsx
@@ -63,43 +63,43 @@ function Carousel() {
     >
       <Slider {...settings}>
         <TbBrandReact
-          className="transition-all duration-500 hover:scale-90 hover:rotate-360 cursor-pointer text-cyan-300"
+          className="transition-all duration-500 hover:scale-90 hover:rotate-360 select-none text-cyan-300"
           title="React"
           aria-label="React" // Acessibilidade: Aria-label diretamente no ícone
           aria-hidden="true"
         />
         <TbBrandNextjs
-          className="transition-all duration-500 hover:scale-90 hover:rotate-360 cursor-pointer text-white"
+          className="transition-all duration-500 hover:scale-90 hover:rotate-360 select-none text-white"
           title="Next.js"
           aria-label="Next.js" // Acessibilidade: Aria-label diretamente no ícone
           aria-hidden="true"
         />
         <TbBrandTailwind
-          className="transition-all duration-500 hover:scale-90 hover:rotate-360 cursor-pointer text-sky-300"
+          className="transition-all duration-500 hover:scale-90 hover:rotate-360 select-none text-sky-300"
           title="Tailwind CSS"
           aria-label="Tailwind CSS" // Acessibilidade: Aria-label diretamente no ícone
           aria-hidden="true"
         />
         <TbBrandTypescript
-          className="transition-all duration-500 hover:scale-90 hover:rotate-360 cursor-pointer text-blue-500"
+          className="transition-all duration-500 hover:scale-90 hover:rotate-360 select-none text-blue-500"
           title="Typescript"
           aria-label="Typescript" // Acessibilidade: Aria-label diretamente no ícone
           aria-hidden="true"
         />
         <TbBrandJavascript
-          className="transition-all duration-500 hover:scale-90 hover:rotate-360 cursor-pointer text-yellow-300"
+          className="transition-all duration-500 hover:scale-90 hover:rotate-360 select-none text-yellow-300"
           title="Javascript"
           aria-label="Javascript" // Acessibilidade: Aria-label diretamente no ícone
           aria-hidden="true"
         />
         <TbBrandCss3
-          className="transition-all duration-500 hover:scale-90 hover:rotate-360 cursor-pointer text-purple-400"
+          className="transition-all duration-500 hover:scale-90 hover:rotate-360 select-none text-purple-400"
           title="CSS3"
           aria-label="CSS3" // Acessibilidade: Aria-label diretamente no ícone
           aria-hidden="true"
         />
         <TbBrandHtml5
-          className="transition-all duration-500 hover:scale-90 hover:rotate-360 cursor-pointer text-orange-400"
+          className="transition-all duration-500 hover:scale-90 hover:rotate-360 select-none text-orange-400"
           title="HTML5"
           aria-label="HTML5" // Acessibilidade: Aria-label diretamente no ícone
           aria-hidden="true"
